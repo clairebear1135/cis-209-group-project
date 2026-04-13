131a07d36230867dee765117d5ca8bc34e92e289
clairebear1135 <claire_bear1135@yahoo.com>
2026-04-13 10:26:21 -0500

    iss5: added hours, contact us and ensured readability

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-console.log("Black Magnolia site loaded");
+<script>
+  const cart = [];
+
+  document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("add-to-cart")) {
+      const product = e.target.dataset.product;
+      cart.push(product);
+      console.log("Cart:", cart);
+
+      const counter = document.querySelector(".cart-count");
+      if (counter) counter.textContent = cart.length;
+    }
+  });
+</script>
